9978e8612e2f4e0eb53a0392f522d65bdf5b78e5
Bartosz Bok <bartos.bok@gmail.com>
2022-06-16 14:51:37 +0200

    tst

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ probability = 0.5
 
 probability_str = str(probability)
 
+@app.get("/items/{item_id}")
+def read_root(item_id: str, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "item_id": item_id}
+
 @app.get("/")
 def hello():
     return {"number":probability_str}
