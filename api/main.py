d8ae19ad11f65076420ea348a9e272d66a591e51
Bartosz Bok <bartos.bok@gmail.com>
2022-06-16 23:45:45 +0200

    feat: update main.py & add model

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
-from typing import Union
-from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
+from joblib import load
+import pandas as pd
 
 app = FastAPI()
-
 
 class Item(BaseModel):
     bmi: int
@@ -27,5 +26,61 @@ class Item(BaseModel):
 
 @app.post("/")
 def response(item: Item):
-    wynik = item.sleepTime * item.bmi
-    return { "result": wynik }
+
+    bmi = item.bmi
+    smoking = item.smoking
+    alcohol = item.alcohol
+    stroke = item.stroke
+    physicalHealth = item.physicalHealth
+    mentalHealth = item.mentalHealth
+    diffWalking = item.diffWalking
+
+    if item.sex == 'Female':
+        sex = 0
+    elif item.sex == 'Male':
+        sex = 1
+
+    age = item.age
+
+    if item.race == 'White':
+        race = 4
+    elif item.race == 'Asian':
+        race = 3
+    elif item.race == 'Hispanic':
+        race = 2
+    elif item.race == 'Other':
+        race = 1
+    elif item.race == 'Black':
+        race = 0
+
+    diabetic = item.diabetic
+    physicalActivity = item.physicalActivity
+
+    if item.genHealth == 'Excellent':
+        genHealth = 4
+    elif item.genHealth == 'Very Good':
+        genHealth = 3
+    elif item.genHealth == 'Good':
+        genHealth = 2
+    elif item.genHealth == 'Fair':
+        genHealth = 1
+    elif item.genHealth == 'Poor':
+        genHealth = 0
+
+    sleepTime = item.sleepTime
+    asthma = item.asthma
+    kidneyDisease = item.kidneyDisease
+    skinCancer = item.skinCancer
+
+    X = [bmi, smoking, alcohol, stroke, physicalHealth, mentalHealth, diffWalking, sex,
+         age, race, diabetic, physicalActivity, genHealth, sleepTime, asthma, kidneyDisease, skinCancer]
+
+    val_X = pd.DataFrame(X)
+    val_X = val_X.values.reshape(1,-1)
+
+    clf = load('model.joblib')
+
+    wynik = clf.predict_proba(val_X)
+    wynik2 = int(wynik[0][1]*100)
+
+    return { "result": wynik2 }
