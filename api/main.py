f04cdf428f5d51abf8f639becd1972d568783e93
Bartosz Bok <bartos.bok@gmail.com>
2022-06-16 14:45:38 +0200

    test

--- api/main.py
+++ api/main.py
@@ -2,6 +2,10 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+probability = 0.5
+
+probability_str = str(probability)
+
 @app.get("/")
 def hello():
-    return {"message":"Siema zjeby, udało się!"}
+    return {"number":probability_str}
