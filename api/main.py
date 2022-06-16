17fc276b66d77a40e56d9c9cee1d7158e04a6aab
Bartosz Bok <bartos.bok@gmail.com>
2022-06-17 00:11:05 +0200

    komentowanie

--- api/main.py
+++ api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from joblib import load
-import pandas as pd
+#from joblib import load
+#import pandas as pd
 
 
 
@@ -95,16 +95,16 @@ def response(item: Item):
     X = [bmi, smoking, alcohol, stroke, physicalHealth, mentalHealth, diffWalking, sex,
          age, race, diabetic, physicalActivity, genHealth, sleepTime, asthma, kidneyDisease, skinCancer]
 
-    val_X = pd.DataFrame(X)
+    #val_X = pd.DataFrame(X)
     #val_X = val_X.values.reshape(1,-1)
 
 
-    clf = load('model.joblib')
+    #clf = load('model.joblib')
 
     print(pd.__version__)
 
-    wynik = clf.predict_proba(val_X)
-    wynik2 = int(wynik[0][1]*100)
+    #wynik = clf.predict_proba(val_X)
+    #wynik2 = int(wynik[0][1]*100)
 
     #clf = load('model.joblib')
 
