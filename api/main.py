5a196dda504ffb63fa1505f3e487100e1f854b8f
Dawid Piechota <53833068+DawidPiechota@users.noreply.github.com>
2022-06-16 22:56:45 +0200

    wtf: i have no idea

--- api/main.py
+++ api/main.py
@@ -1,9 +1,27 @@
 from typing import Union
 from enum import Enum
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:8080",
+    "https://localhost:8080",
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Item(BaseModel):
